75c2485e68321643dae4d76dc32efabf93274b12
Pharohx <usepurpose642@gmail.com>
2025-06-24 21:12:11 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+
+from flask import Flask, render_template, request, redirect, send_from_directory
+import os
+import yt_dlp
+
+app = Flask(__name__)
+DOWNLOAD_FOLDER = "downloads"
+os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        url = request.form['url']
+        if url:
+            ydl_opts = {
+                'outtmpl': f'{DOWNLOAD_FOLDER}/%(title)s.%(ext)s',
+                'format': 'best'
+            }
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(url, download=True)
+                filename = ydl.prepare_filename(info)
+                return send_from_directory(DOWNLOAD_FOLDER, os.path.basename(filename), as_attachment=True)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
